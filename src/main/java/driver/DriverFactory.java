12cb6dd187967eede43d90e1fc95044ea64065b9
Alina <alinalosovskaya@gmail.com>
2026-09-03 13:32:22 +0300

    Add log4j2 logging: console + daily rolling file

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,15 +1,21 @@
 package driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
 
+    private static final Logger LOGGER = LogManager.getLogger(DriverFactory.class);
+
     public static WebDriver createDriver(String browserName) {
 
         String browser = browserName == null ? "chrome" : browserName.toLowerCase();
+
+        LOGGER.info("Creating WebDriver for browser: {}", browser);
 
         WebDriver driver;
 
@@ -23,8 +29,11 @@ public class DriverFactory {
                 driver = new EdgeDriver();
                 break;
             default:
+                LOGGER.error("Unsupported browser requested: {}", browser);
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+
+        LOGGER.debug("WebDriver instance created: {}", driver);
 
         return driver;
     }
