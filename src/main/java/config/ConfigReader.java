5cf90d671c62ae0d523e16ec46a919daa7559441
Alina <alinalosovskaya@gmail.com>
2026-09-03 03:50:36 +0300

    Add ConfigReader and qa/dev property files

---
+++ src/main/java/config/ConfigReader.java
@@ -0,0 +1,39 @@
+package config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static final String ENV = System.getProperty("env", "qa");
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        String fileName = "config/" + ENV + ".properties";
+
+        try (InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream(fileName)) {
+
+            if (input == null) {
+                throw new RuntimeException("Config file not found: " + fileName);
+            }
+
+            PROPERTIES.load(input);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load config file: " + fileName, e);
+        }
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    public static String baseUrl() {
+        return get("base.url");
+    }
+
+    public static String browser() {
+        return get("browser");
+    }
+}
